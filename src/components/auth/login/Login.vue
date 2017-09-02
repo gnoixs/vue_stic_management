fd8d8142080b7503184ff187ccde839172257163
feiker <feiker.xr@gmail.com>
2017-09-02 17:09:47 +0800

    init

---
+++ src/components/auth/login/Login.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="login">
+    <h2>管理端登录</h2>
+    <form method="post" action="/auth/login" name="login">
+      <div class="form-group">
+        <div class="input-group">
+          <input type="text" id="username" required="required" v-model="User.user_name"/>
+          <label class="control-label" for="username">用户名</label><i class="bar"></i>
+        </div>
+      </div>
+      <div class="form-group margin-bottom-less">
+        <div class="input-group">
+          <input type="password" id="password" required="required" v-model="User.user_passwd"/>
+          <label class="control-label" for="password">密码</label><i class="bar"></i>
+        </div>
+      </div>
+      <p class="warning" v-text="errorMsg" :style="{visibility: !errorMsg}"></p>
+      <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between down-container">
+        <button class="btn btn-primary" type="submit" @click.prevent.stop="onsubmit" :disabled="!isRightInput">
+          登录
+        </button>
+        <router-link class='link' :to="{name: 'Signup'}">创建账户</router-link>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  //import Modals from '@/components/ui/modals/Modals'
+  import axios from 'axios'
+  export default {
+    name: 'login',
+    data () {
+      return {
+        User: {         // 用户对象，存储用户名和密码
+          user_name: '',
+          user_passwd: ''
+        },
+        isRightInput: true,    // 是不是可以正确提交
+        errorMsg: ''
+      }
+    },
+    methods: {
+
+      onsubmit () {
+        console.log(1);
+        axios.post('http://adm.leyoule1.com/api/login',
+          this.User,
+          {
+            headers: {
+              "authKey": 'longwords'
+            }
+          }).then((res) => {
+          console.log(res.data)
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../../../sass/variables';
+  @import '../../../../node_modules/bootstrap/scss/mixins/breakpoints';
+  @import '../../../../node_modules/bootstrap/scss/variables';
+
+  .login {
+    @include media-breakpoint-down(md) {
+      width: 100%;
+      padding-right: 2rem;
+      padding-left: 2rem;
+      .down-container {
+        .link {
+          margin-top: 2rem;
+        }
+      }
+    }
+    // password text
+    .margin-bottom-less {
+      margin-bottom: 0.75rem;
+    }
+
+    h2 {
+      text-align: center;
+    }
+    width: 21.375rem;
+    .down-container {
+      margin-top: 3.125rem;
+    }
+    .warning {
+      color: red;
+      font-size: 0.95rem;
+      font-family: serif;
+      position: absolute;
+    }
+  }
+</style>
---
+++ src/components/auth/signup/Signup.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="signup">
+    <h2>创建管理员账户</h2>
+    <form method="post" action="/auth/signup" name="signup">
+      <div class="form-group">
+        <div class="input-group">
+          <input type="text" id="email" required="required"/>
+          <label class="control-label" for="email">用户名</label><i class="bar"></i>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="input-group">
+          <input type="password" id="password" required="required"/>
+          <label class="control-label" for="password">密码</label><i class="bar"></i>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="input-group">
+          <input type="password" id="password2" required="required"/>
+          <label class="control-label" for="password">确认密码</label><i class="bar"></i>
+        </div>
+      </div>
+      <div class="abc-checkbox abc-checkbox-primary">
+        <input id="checkbox1" type="checkbox" checked>
+        <label for="checkbox1">
+          <span class="abc-label-text">我同意以下 <router-link to="">注册条款</router-link></span>
+        </label>
+      </div>
+      <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between down-container">
+        <button class="btn btn-primary" type="submit">
+          注册
+        </button>
+        <router-link class='link' :to="{name: 'Login'}">已有账户?</router-link>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'signup'
+  }
+</script>
+
+<style lang="scss">
+  @import '../../../sass/variables';
+  @import '../../../../node_modules/bootstrap/scss/mixins/breakpoints';
+  @import '../../../../node_modules/bootstrap/scss/variables';
+
+  .signup {
+    @include media-breakpoint-down(md) {
+      width: 100%;
+      padding-right: 2rem;
+      padding-left: 2rem;
+      .down-container {
+        .link {
+          margin-top: 2rem;
+        }
+      }
+    }
+
+    h2 {
+      text-align: center;
+    }
+    width: 21.375rem;
+
+    .down-container {
+      margin-top: 2.6875rem;
+    }
+  }
+</style>
